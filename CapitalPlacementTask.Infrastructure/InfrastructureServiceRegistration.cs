a4940db26c6f1af5bf2d90d1c2fe395590b4b9f9
successJohn <successojohn@gmail.com>
2024-05-16 21:15:38 +0100

    Configured cosmos db

---
+++ CapitalPlacementTask.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacementTask.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static void ConfigureCosmosDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton((provider) =>
+            {
+                var endpointUri = configuration["CosmosDbSettings:EndpointUri"];
+                var primaryKey = configuration["CosmosDbSettings:PrimaryKey"];
+                var databaseName = configuration["CosmosDbSettings:DatabaseName"];
+
+                var cosmosClientOptions = new CosmosClientOptions
+                {
+                    ApplicationName = databaseName,
+                    ConnectionMode = ConnectionMode.Gateway,
+                };
+               
+                var cosmosClient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+
+
+                return cosmosClient;
+            });
+        }
+    }
+}
---
+++ CapitalPlacementTask.Infrastructure/Services/ProgramFormService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacementTask.Infrastructure.Services
+{
+    public class ProgramFormService
+    {
+        private readonly Container _programDetailContainer;
+
+        public ProgramFormService(CosmosClient cosmosClient, IConfiguration configuration)
+        {
+            var databaseName = configuration["CosmosDbSettings:DatabaseName"];
+            _programDetailContainer = cosmosClient.GetContainer(databaseName, "ProgramDetail");
+        }
+        public async Task<Guid> CreateProgram(CreateProgramDTO model)
+        {
+            var programId = Guid.NewGuid();
+            var personalInfoId = Guid.NewGuid();
+            var program = new ProgramDetail
+            {
+                id = programId,
+                Title = model.Title,
+                Description = model.Description,
+
+                personalInformation = new PersonalInformation
+                {
+                    Id = personalInfoId,
+                    FirstName = model.personalInformation?.FirstName,
+                    LastName = model.personalInformation?.LastName,
+                    Email = model.personalInformation?.Email,
+                    Nationality = model.personalInformation?.Nationality,
+                    DateOfBirth = model.personalInformation!.DateOfBirth,
+                    IdNumber = model.personalInformation?.IdNumber,
+                    CurrentResidence = model.personalInformation?.CurrentResidence
+
+                },
+                PersonalInformationId = personalInfoId/*,
+                CustomQuestions = model.createQuestionDTO.Select(x => new Question
+                {
+
+                }).ToList(),*/
+
+            };
+
+            await _programDetailContainer.CreateItemAsync(program);
+
+            return program.id;
+        }
+
+        public async Task<ProgramDetail> GetApplicationForm(Guid id)
+        {
+
+            var applicationForm = await _programDetailContainer.ReadItemAsync<ProgramDetail>(id.ToString(), new PartitionKey(id.ToString()));
+
+
+            return applicationForm.Resource;
+
+        }
+    }
+}
