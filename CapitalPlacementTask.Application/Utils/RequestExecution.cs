3a6298a5d21b4197571fa1c1368a9a064c518348
successJohn <successojohn@gmail.com>
2024-05-16 21:43:29 +0100

    Added custom return message

---
+++ CapitalPlacementTask.Application/Utils/RequestExecution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacementTask.Application.Utils
+{
+    public enum RequestExecution
+    {
+        Successful = 1,
+        Failed,
+        Error
+    }
+}
